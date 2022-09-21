d763effeadd97736cd39e63b8fef485aa6d28879
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2022-09-21 01:59:38 -0700

    now we're making some tables.

--- ee521_powerFlowAnalysis.jl
+++ ee521_powerFlowAnalysis.jl
@@ -1,14 +1,30 @@
-using DataVoyager
-using VegaDatasets
-using VegaLite
 using CSV
+folderInput = "rawData/"
+#inputData = CSV.read(folderInput*"ieee14cdf")
 
-data = dataset("iris")
-print(data)
-vscodedisplay(data)
+brand = ["Company A", "Company A", "Company A"]
+tier = ["Premium", "Economy", "Standard"]
+quantity = [10, 100, 50]
+price = [100, 10, 50]
+cost = [30, 7, 25]
 
-v = Voyager(data)
-
-p = v[]
-save("iris_voyager.png", p)
-save("iris_voyager.svg", p)
+mymatrix = [brand tier quantity price cost]
+myprice = mymatrix[:, 4]
+myeconomy = mymatrix[2, :]
+myquantity = mymatrix[:, 3]
+myrevenue = myquantity .* myprice
+mymatrix = [mymatrix myrevenue]
+mycost = mymatrix[:, 5]
+myprofit = myquantity .* (myprice - mycost)
+mymargin = myprofit ./ myrevenue
+mymatrix = [mymatrix myprofit]
+mytotalquantity = sum(myquantity)
+mytotalrevenue = sum(myrevenue)
+mytotalprofit = sum(myprofit)
+mytotalmargin = mytotalprofit / mytotalrevenue
+metrics = ["Quantity", "Revenue", "Profit", "Margin"]
+totals = [mytotalquantity, mytotalrevenue, mytotalprofit, mytotalmargin]
+mytotalmatrix = [metrics totals]
+mymatrix = [mymatrix mymargin]
+using DelimitedFiles
+writedlm("mymatrix.csv", mymatrix, ',')
