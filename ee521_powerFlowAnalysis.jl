e45082f7c44c1ae499b390f0ee1081250355704e
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2022-09-21 10:05:29 -0700

    header data inserted.

--- ee521_powerFlowAnalysis.jl
+++ ee521_powerFlowAnalysis.jl
@@ -1,30 +1,18 @@
 using CSV
+using DelimitedFiles, DataFrames
+
 folderInput = "rawData/"
 #inputData = CSV.read(folderInput*"ieee14cdf")
 
-brand = ["Company A", "Company A", "Company A"]
-tier = ["Premium", "Economy", "Standard"]
-quantity = [10, 100, 50]
-price = [100, 10, 50]
-cost = [30, 7, 25]
+dataInput, header = readdlm(joinpath(folderInput
+,"ieee14cdf.txt"), header = true)
 
-mymatrix = [brand tier quantity price cost]
-myprice = mymatrix[:, 4]
-myeconomy = mymatrix[2, :]
-myquantity = mymatrix[:, 3]
-myrevenue = myquantity .* myprice
-mymatrix = [mymatrix myrevenue]
-mycost = mymatrix[:, 5]
-myprofit = myquantity .* (myprice - mycost)
-mymargin = myprofit ./ myrevenue
-mymatrix = [mymatrix myprofit]
-mytotalquantity = sum(myquantity)
-mytotalrevenue = sum(myrevenue)
-mytotalprofit = sum(myprofit)
-mytotalmargin = mytotalprofit / mytotalrevenue
-metrics = ["Quantity", "Revenue", "Profit", "Margin"]
-totals = [mytotalquantity, mytotalrevenue, mytotalprofit, mytotalmargin]
-mytotalmatrix = [metrics totals]
-mymatrix = [mymatrix mymargin]
-using DelimitedFiles
-writedlm("mymatrix.csv", mymatrix, ',')
+headerBusData = ["bus", "busName", "voltageLevel", "loadFlowArea", "lossZone",
+"busType", "vFinal","deltaFinal", "P_L", "Q_L", "P_G", "Q_G", "kVBase", "VSet",
+"vSet", "Q_Max", "Q_Min", "G", "B", "remoteControlledBusNumber"]
+
+headerBranchData = ["i", "j", "loadFlowArea", "lossZone", "ckt", "type", 
+"R", "X", "B", "MVA1", "MVA2", "MVA3", "controlBusNumber", "side", "a", "phi",
+"aMin_or_phiMin", "aMax_or_phiMax", "stepSize", "Q_Max", "Q_Min"]
+
+vscodedisplay(dataInput)
