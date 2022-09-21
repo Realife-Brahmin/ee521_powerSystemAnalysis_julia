c037e9ab0dbf7e33e30a1fdddcd1ce1b02f33439
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2022-09-21 01:24:13 -0700

    A beautiful graph before it is all removed :(

--- ee521_powerFlowAnalysis.jl
+++ ee521_powerFlowAnalysis.jl
@@ -1,7 +1,14 @@
 using DataVoyager
 using VegaDatasets
 using VegaLite
+using CSV
 
 data = dataset("iris")
 print(data)
 vscodedisplay(data)
+
+v = Voyager(data)
+
+p = v[]
+save("iris_voyager.png", p)
+save("iris_voyager.svg", p)
